ad00fe4eb8f78eb77615a13117c4eb06d6a61b9c
satyam4321 <satyam@an-associates.com>
2023-11-11 22:13:48 +0530

    test-1

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
-const botName = "ChatCord Bot";
+const botName = "ConvoHub Bot";
 
 // Run when client connects
 io.on("connection", (socket) => {
@@ -32,7 +32,7 @@ io.on("connection", (socket) => {
     socket.join(user.room);
 
     // Welcome current user
-    socket.emit("message", formatMessage(botName, "Welcome to ChatCord!"));
+    socket.emit("message", formatMessage(botName, "Welcome to ConvoHub!"));
 
     // Broadcast when a user connects
     socket.broadcast
